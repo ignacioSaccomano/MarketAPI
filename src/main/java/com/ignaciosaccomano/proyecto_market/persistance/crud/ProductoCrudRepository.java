f57cb1a9c7a7801102805b2e42298ebe84a43d35
Ignacio Saccomano <ignacio.saccomano@gmail.com>
2025-02-04 21:10:16 -0300

    Query methods de prueba

---
+++ src/main/java/com/ignaciosaccomano/proyecto_market/persistance/crud/ProductoCrudRepository.java
@@ -0,0 +1,12 @@
+package com.ignaciosaccomano.proyecto_market.persistance.crud;
+
+import com.ignaciosaccomano.proyecto_market.persistance.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}

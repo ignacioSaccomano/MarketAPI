0ac601065f6ae9c91d1719eaf736c21aeab5e24a
Ignacio Saccomano <ignacio.saccomano@gmail.com>
2025-02-10 16:55:21 -0300

    Creo modelo de dominio de compras e implemento response entities para el product controller

---
+++ src/main/java/com/ignaciosaccomano/proyecto_market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.ignaciosaccomano.proyecto_market.domain.repository;
+
+import com.ignaciosaccomano.proyecto_market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}

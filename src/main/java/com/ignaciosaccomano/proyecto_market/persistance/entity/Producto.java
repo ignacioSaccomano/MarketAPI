2a1515faca1e4942c55b178f69b1a764165352de
Ignacio Saccomano <ignacio.saccomano@gmail.com>
2025-02-10 02:47:49 -0300

    Agrego getters y setters que faltaban y finalizo el product controller

--- src/main/java/com/ignaciosaccomano/proyecto_market/persistance/entity/Producto.java
+++ src/main/java/com/ignaciosaccomano/proyecto_market/persistance/entity/Producto.java
@@ -27,12 +27,20 @@ public class Producto {
 
     private Boolean estado;
 
+
     @ManyToOne
     @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
     private Categoria categoria;
 
     // Seteamos getters y setters
 
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
     public Integer getIdProducto() {
         return idProducto;

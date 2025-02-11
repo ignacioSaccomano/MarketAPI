362c5651bc807730598e41f822cf9cb7eb1a9a52
Ignacio Saccomano <ignacio.saccomano@gmail.com>
2025-02-11 19:10:40 -0300

    Implemento swagger para documentacion del api

--- src/main/java/com/ignaciosaccomano/proyecto_market/web/controller/ProductController.java
+++ src/main/java/com/ignaciosaccomano/proyecto_market/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ignaciosaccomano.proyecto_market.web.controller;
 
 import com.ignaciosaccomano.proyecto_market.domain.Product;
 import com.ignaciosaccomano.proyecto_market.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +18,13 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @Operation(description = "Get all supermarket products.")
     public ResponseEntity<List<Product>> getAll(){
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "Search a product by ID")
     public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
